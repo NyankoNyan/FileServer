e86d3c5077edb7653de2660270182d8890a2f9b1
Nyanko <gverycrazy@gmail.com>
2023-12-29 12:43:25 +0300

    RESTful

--- source/run.py
+++ source/run.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
-from flask import Flask, jsonify, request, send_file
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from datetime import timedelta
+from flask import Flask, jsonify, request, send_file, make_response
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
 import json
 import os
 import shutil
@@ -57,6 +58,8 @@ with open(users_path) as f:
 app = Flask(__name__)
 jwt = JWTManager(app)
 app.config["JWT_SECRET_KEY"] = "this-is-secret-key"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=8)
+jwt_blocklist = set()
 
 
 def add_slash(s):
@@ -115,8 +118,35 @@ def get_user_groups() -> list(str):
         return user.groups
 
 
-@app.route(config['url_prefix'] + '/login', methods=['GET'])
-def fs_login():
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    jti = jwt_payload["jti"]
+    return jti in jwt_blocklist
+
+
+@app.route(config['url_prefix'] + '/<path:url>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@jwt_required(optional=True)
+def fs_request(url):
+    if url == 'login':
+        if request.method != 'GET':
+            return jsonify(msg='Bad request method'), 400
+        return login()
+    elif url == 'logout':
+        if request.method != 'GET':
+            return jsonify(msg='Bad request method'), 400
+        return logout()
+    else:
+        if request.method == 'GET':
+            return get_file_or_file_list(url)
+        elif request.method == 'DELETE':
+            return delete(url)
+        elif request.method in ('POST', 'PUT'):
+            return post_put(url)
+        else:
+            return jsonify(msg='Bad request method'), 400
+
+
+def login():
     if request.is_json:
         user_name = request.json['user']
     else:
@@ -125,7 +155,7 @@ def fs_login():
     user = users.get(user_name)
     if user is None:
         print(f'Unknown user {user_name} login failure')
-        return 'Login failure', 400
+        return jsonify(msg='Login failure'), 400
 
     if user.auth_method == 'password':
         if request.is_json:
@@ -134,130 +164,155 @@ def fs_login():
             user_password = request.form['password']
         if user_password != user.password:
             print(f'Bad password for {user.name}')
-            return 'Login faulure', 400
+            return jsonify(msg='Login failure'), 400
     else:
         print(f'Unknown auth method {user.auth_method} for {user.name}')
-        return 'Login faulure', 400
+        return jsonify(msg='Login failure'), 400
 
     token = create_access_token(identity=user.name)
     print(f'User {user.name} succesful login')
-    return jsonify(message='Login succesful!', access_token=token)
+    return jsonify(msg='Login succesful!', access_token=token), 200
 
 
-@app.route(config['url_prefix'] + '/list', methods=['GET'])
-@jwt_required(optional=True)
-def fs_list():
-    try:
-        storage = get_storage()
-        folder = get_filepath(storage)
-    except RequestError as e:
-        return e.args[0], 400
-
-    groups = get_user_groups()
-    if not storage.check_read(groups):
-        print(f'Permisisons deny {get_jwt_identity()} {storage.name}')
-        return 'Permissions deny', 400
-
-    if not os.path.isdir(folder):
-        print(f"Missing storage directory {folder}")
-        return 'Internal server error', 400
-
-    result = []
-    for content in os.listdir(folder):
-        fullpath = folder + '/' + content
-        result.append({
-            "path": content,
-            "isDir": os.path.isdir(fullpath)
-        })
-
-    return jsonify(result)
+def logout():
+    payload = get_jwt()
+    if payload is None:
+        return jsonify(msg='You are not logining'), 400
+    jti = payload.get('jti')
+    if jti is None:
+        return jsonify(msg='You are not logining'), 400
+    if str(jti) in jwt_blocklist:
+        return jsonify(msg='You are not logining'), 400
+    jwt_blocklist.add(str(jti))
+    user_id = get_jwt_identity()
+    print(f'User {user_id} succesful logout')
+    return jsonify(msg='Logout succesfull'), 200
 
 
-@app.route(config['url_prefix'] + '/read', methods=['GET'])
-@jwt_required(optional=True)
-def fs_read():
-    try:
-        storage = get_storage()
-        fullpath = get_filepath(storage)
-    except RequestError as e:
-        return e.args[0], 400
+def get_file_or_file_list(url: str):
+    first_slash = url.find('/')
+    if first_slash == -1:
+        storage_name = url
+        path = ''
+    else:
+        storage_name = url[0:first_slash]
+        path = url[first_slash+1:]
+
+    storage = storages.get(storage_name)
+    if storage is None:
+        return jsonify(msg='Not found'), 404
 
     groups = get_user_groups()
     if not storage.check_read(groups):
         print(f'Permisisons deny {get_jwt_identity()} {storage.name}')
-        return 'Permissions deny', 400
+        return jsonify(msg='Permissions deny'), 400
+
+    if len(path) == 0:
+        fullpath = storage.path
+    else:
+        fullpath = storage.path + '/' + path
+    if fullpath != os.path.normpath(fullpath):
+        return jsonify(msg='Not found'), 404
 
     if not os.path.exists(fullpath):
-        return 'File not found '+fullpath, 400
+        return jsonify(msg='Not found'), 404
 
-    return send_file(fullpath, as_attachment=True)
-
-
-@app.route(config['url_prefix'] + '/mkdir', methods=['POST'])
-@jwt_required(optional=True)
-def fs_mkdir():
-    try:
-        storage = get_storage()
-        fullpath = get_filepath(storage)
-    except RequestError as e:
-        return e.args[0], 400
-
-    groups = get_user_groups()
-    if not storage.check_write(groups):
-        print(f'Permisisons deny {get_jwt_identity()} {storage.name}')
-        return 'Permissions deny', 400
-
-    if os.path.exists(fullpath):
-        return "Directory already exists", 400
-
-    os.mkdir(fullpath)
-
-    return "Created", 200
+    if os.path.isdir(fullpath):
+        result = []
+        for content in os.listdir(fullpath):
+            subpath = fullpath + '/' + content
+            result.append({
+                "path": content,
+                "is_dir": os.path.isdir(subpath)
+            })
+        return jsonify(msg='Directory list', files=result), 200
+    else:
+        return send_file(fullpath, as_attachment=True)
 
 
-@app.route(config['url_prefix'] + '/write', methods=['POST'])
-@jwt_required(optional=True)
-def fs_write():
-    try:
-        storage = get_storage()
-        fullpath = get_filepath(storage)
-        file = get_file_content()
-    except RequestError as e:
-        return e.args[0], 400
+def delete(url: str):
+    first_slash = url.find('/')
+    if first_slash == -1:
+        storage_name = url
+        path = ''
+    else:
+        storage_name = url[0:first_slash]
+        path = url[first_slash+1:]
+
+    storage = storages.get(storage_name)
+    if storage is None:
+        return jsonify(msg='Not found'), 404
 
     groups = get_user_groups()
     if not storage.check_write(groups):
         print(f'Permisisons deny {get_jwt_identity()} {storage.name}')
-        return 'Permissions deny', 400
+        return jsonify(msg='Permissions deny'), 400
 
-    file.save(fullpath)
-
-    return "Saved", 200
-
-
-@app.route(config['url_prefix'] + '/del', methods=['DELETE'])
-@jwt_required(optional=True)
-def fs_del():
-    try:
-        storage = get_storage()
-        fullpath = get_filepath(storage)
-    except RequestError as e:
-        return e.args[0], 400
-
-    groups = get_user_groups()
-    if not storage.check_write(groups):
-        print(f'Permisisons deny {get_jwt_identity()} {storage.name}')
-        return 'Permissions deny', 400
+    fullpath = storage.path + '/' + path
+    if fullpath != os.path.normpath(fullpath):
+        return jsonify(msg='Not found'), 404
 
     if not os.path.exists(fullpath):
-        return 'Path not exists', 400
+        return jsonify(msg='Not found'), 404
 
     if os.path.isdir(fullpath):
         shutil.rmtree(fullpath)
-        return 'Directory removed', 200
+        return jsonify(msg='Directory removed'), 200
     else:
         os.remove(fullpath)
-        return 'File removed', 200
+        return jsonify(msg='File removed'), 200
+
+
+def post_put(url: str):
+    first_slash = url.find('/')
+    if first_slash == -1:
+        storage_name = url
+        path = ''
+    else:
+        storage_name = url[0:first_slash]
+        path = url[first_slash+1:]
+
+    storage = storages.get(storage_name)
+    if storage is None:
+        return jsonify(msg='Not found'), 404
+
+    groups = get_user_groups()
+    if not storage.check_write(groups):
+        print(f'Permisisons deny {get_jwt_identity()} {storage.name}')
+        return jsonify(msg='Permissions deny'), 400
+
+    fullpath = storage.path + '/' + path
+    if fullpath != os.path.normpath(fullpath):
+        return jsonify(msg='Not found'), 404
+
+    if len(request.files) == 0:
+        if os.path.exists(fullpath):
+            if request.method == 'POST':
+                return jsonify(msg='Already exists'), 400
+            elif os.path.isdir(fullpath):
+                return jsonify(msg='Already exists'), 200
+            else:
+                os.remove(fullpath)
+                os.mkdir(fullpath)
+                return jsonify(msg='Created'), 200
+        else:
+            os.mkdir(fullpath)
+            return jsonify(msg='Created'), 200
+    else:
+        if os.path.exists(fullpath):
+            if request.method == 'POST':
+                return jsonify(msg='Already exists'), 400
+            else:
+                if os.path.isdir(fullpath):
+                    shutil.rmtree(fullpath)
+                else:
+                    os.remove(fullpath)
+        file = get_file_content()
+        file.save(fullpath)
+        response = make_response(jsonify(msg='Created'))
+        response.headers.add('Location', storage.name + '/' + path)
+        response.status_code = 200
+        return response
 
 
 if __name__ == '__main__':
--- test/alltests.py
+++ test/alltests.py
@@ -14,54 +14,58 @@ def run_all_tests():
     check_file_exists()
     remove_folder()
     check_folder_exists()
+    deny_token()
 
 
 def request_token():
-    r = requests.get('http://localhost:5000/fs/login',
+    r = requests.get('http://localhost:5000/login',
                      data={'user': 'admin', 'password': 'admin'})
-    if r.status_code != 200:
-        print(f'request_token: {r.status_code}: {r.text}')
-        return
-    global access_token
-    access_token = json.loads(r.text)['access_token']
+    print(f'request_token: {r.status_code}: {r.text}')
+    if r.status_code == 200:
+        global access_token
+        access_token = json.loads(r.text)['access_token']
+
+
+def deny_token():
+    r = requests.get('http://localhost:5000/logout',
+                     headers={'Authorization': 'Bearer '+access_token})
+    print(f'deny_token: {r.status_code}: {r.text}')
 
 
 def create_folder():
-    r = requests.post('http://localhost:5000/fs/mkdir?s=test&f=test_folder',
+    r = requests.post('http://localhost:5000/test/test_folder',
                       headers={'Authorization': 'Bearer '+access_token})
-    if r.status_code != 200:
-        print(f'create_folder: {r.status_code}: {r.text}')
+    print(f'create_folder: {r.status_code}: {r.text}')
 
 
 def upload_file_to_folder():
-    r = requests.post('http://localhost:5000/fs/write?s=test&f=test_folder%2ftest_file.txt',
+    r = requests.post('http://localhost:5000/test/test_folder/test_file.txt',
                       headers={'Authorization': 'Bearer '+access_token},
                       files={'upload_file': open('test_file.txt', 'rb')}
                       )
-    if r.status_code != 200:
-        print(f'upload_file_to_folder: {r.status_code}: {r.text}')
+    print(f'upload_file_to_folder: {r.status_code}: {r.text}')
 
 
 def get_file_list():
-    r = requests.get('http://localhost:5000/fs/list?s=test&f=test_folder',
+    r = requests.get('http://localhost:5000/test/test_folder',
                      headers={'Authorization': 'Bearer '+access_token})
+    print(f'get_file_list: {r.status_code}: {r.text}')
     if r.status_code != 200:
-        print(f'get_file_list: {r.status_code}: {r.text}')
         return
-    files = json.loads(r.text)
+    files = json.loads(r.text)['files']
     if len(files) != 1:
         print(f'get_file_list: Files count {len(files)}')
         return
-    if files[0]['path'] != 'test_file.txt' or files[0]['isDir'] != False:
+    if files[0]['path'] != 'test_file.txt' or files[0]['is_dir'] != False:
         print(f'get_file_list: File content {files[0]}')
 
 
 def download_file():
     r = requests.get(
-        'http://localhost:5000/fs/read?s=test&f=test_folder%2ftest_file.txt',
+        'http://localhost:5000/test/test_folder/test_file.txt',
         headers={'Authorization': 'Bearer '+access_token})
+    print(f'download_file: {r.status_code}: {r.text}')
     if r.status_code != 200:
-        print(f'download_file: {r.status_code}: {r.text}')
         return
     with open('test_file.txt', 'r') as f:
         local_content = f.read()
@@ -73,38 +77,36 @@ def download_file():
 
 def remove_file():
     r = requests.delete(
-        'http://localhost:5000/fs/del?s=test&f=test_folder%2ftest_file.txt',
+        'http://localhost:5000/test/test_folder/test_file.txt',
         headers={'Authorization': 'Bearer '+access_token})
-    if r.status_code != 200:
-        print(f'remove_file: {r.status_code}: {r.text}')
+    print(f'remove_file: {r.status_code}: {r.text}')
 
 
 def check_file_exists():
-    r = requests.get('http://localhost:5000/fs/list?s=test&f=test_folder',
+    r = requests.get('http://localhost:5000/test/test_folder',
                      headers={'Authorization': 'Bearer '+access_token})
+    print(f'check_file_exists: {r.status_code}: {r.text}')
     if r.status_code != 200:
-        print(f'check_file_exists: {r.status_code}: {r.text}')
         return
-    files = json.loads(r.text)
+    files = json.loads(r.text)['files']
     if len(files) != 0:
         print('check_file_exists: Folder is not empty')
 
 
 def remove_folder():
     r = requests.delete(
-        'http://localhost:5000/fs/del?s=test&f=test_folder',
+        'http://localhost:5000/test/test_folder',
         headers={'Authorization': 'Bearer '+access_token})
-    if r.status_code != 200:
-        print(f'remove_folder: {r.status_code}: {r.text}')
+    print(f'remove_folder: {r.status_code}: {r.text}')
 
 
 def check_folder_exists():
-    r = requests.get('http://localhost:5000/fs/list?s=test&f=',
+    r = requests.get('http://localhost:5000/test',
                      headers={'Authorization': 'Bearer '+access_token})
+    print(f'check_folder_exists: {r.status_code}: {r.text}')
     if r.status_code != 200:
-        print(f'check_folder_exists: {r.status_code}: {r.text}')
         return
-    files = json.loads(r.text)
+    files = json.loads(r.text)['files']
     if len(files) != 0:
         print('check_file_exists: Folder is not empty')
 
